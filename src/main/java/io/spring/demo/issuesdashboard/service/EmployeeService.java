81ce32e3ca9b2dede292c901b929d80f8e989c64
rd-spring-coder <riteshdharmatti@gmail.com>
2018-02-07 22:30:59 +0530

    removing service layer

--- src/main/java/io/spring/demo/issuesdashboard/service/EmployeeService.java
+++
@@ -1,9 +0,0 @@
-package io.spring.demo.issuesdashboard.service;
-
-import java.util.List;
-
-import io.spring.demo.issuesdashboard.domain.Employee;
-
-public interface EmployeeService {
-	List<Employee> getAllEmployees();
-}
